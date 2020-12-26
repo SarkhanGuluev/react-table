efd484637373843fbd80c85f9582915b89b9b812
SarkhanGuluev <37984618+SarkhanGuluev@users.noreply.github.com>
2020-12-26 23:17:27 +0300

    Update App.js
    loader

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ console.log(state)
           </div>
           { loading ?
             <div style={{display: 'flex', justifyContent: 'center', margin: '150px',}}>
-            <Loader type="Oval" color="#00BFFF" height={80} width={80}/>
+            <Loader type="Oval" color="#00BFFF" height={110} width={110}/>
             </div> :
             <Table/>
           }
